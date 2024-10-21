4f14c17bed12fccea21e01fa06510e367b90baa4
ainhoavega99 <ainhoavega99@gmail.com>
2024-10-21 17:28:00 +0200

    modo repsonsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
-import { Component, Renderer2 } from '@angular/core';
+import { Component, ElementRef, Renderer2 } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
-import { IndexComponent } from "./index/index.component";
 
 @Component({
   selector: 'app-root',
@@ -10,27 +9,38 @@ import { IndexComponent } from "./index/index.component";
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'portafolio';
 
-  constructor(private renderer: Renderer2) {}
+  constructor(private renderer: Renderer2, private el: ElementRef) { }
 
+  container = document.querySelector('.container');
+  main = document.querySelector('.main');
+  menu = document.querySelector('#menu__toggle');
+
+  ngAfterViewInit() {
+   
+     this.menu = this.el.nativeElement.querySelector('#menu__toggle');
+  }
   toggleMenu(event: any) {
-    const container = document.querySelector('.container');
-    const main = document.querySelector('.main');
+
     if (event.target.checked) {
-      this.renderer.setStyle(container, 'width', '80vw');
-      this.renderer.setStyle(container, 'float', 'right');
-      this.renderer.setStyle(container, 'padding', '0 20px');
-      this.renderer.setStyle(container, 'background-image', 'url("../assets/files/fondo1.jpg")');
-      this.renderer.setStyle(container, 'background-size', 'cover'); 
-      this.renderer.setStyle(container, 'background-position', 'center'); 
+      this.renderer.setStyle(this.container, 'width', '80vw');
+      this.renderer.setStyle(this.container, 'float', 'right');
+      this.renderer.setStyle(this.container, 'padding', '0 20px');
+      this.renderer.setStyle(this.container, 'background-image', 'url("../assets/files/fondo1.jpg")');
+      this.renderer.setStyle(this.container, 'background-size', 'cover');
+      this.renderer.setStyle(this.container, 'background-position', 'center');
     } else {
-      this.renderer.setStyle(container, 'width', '100vw');
-      this.renderer.removeStyle(container, 'background-image');
-      this.renderer.setStyle(container, 'float', 'none');
-      this.renderer.setStyle(main, 'height', '100%');
-      this.renderer.setStyle(main, 'background-size', 'cover');
-      this.renderer.setStyle(main, 'background-position', 'center');
+      this.renderer.setStyle(this.container, 'width', '100vw');
+      this.renderer.removeStyle(this.container, 'background-image');
+      this.renderer.setStyle(this.container, 'float', 'none');
+      this.renderer.setStyle(this.main, 'height', '100%');
+      this.renderer.setStyle(this.main, 'background-size', 'cover');
+      this.renderer.setStyle(this.main, 'background-position', 'center');
     }
+  }
+
+  closeMenu(): void {
+    let bool = this.menu?.checkVisibility
+    this.renderer.setProperty(this.menu, 'checked', !bool);
   }
 }
